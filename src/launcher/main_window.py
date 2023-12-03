83bbcb649821f1bbb90e2e0ba64b098c9efce4ab
izharus <ruslan.izhakovskij@gmail.com>
2023-12-03 20:59:18 +0300

    pre-release: 2.0.0 RC_2

--- src/launcher/main_window.py
+++ src/launcher/main_window.py
@@ -164,7 +164,7 @@ class Window(QtWidgets.QMainWindow):
             self._make_authorization_finished
         )
 
-        self.setWindowTitle("TFC-Halloween 2.0.0 RC-1")
+        self.setWindowTitle("TFC-Halloween 2.0.0 RC-2")
 
         # pylint: disable = C0301
         self._ui_instance.pushButton_minecraft_dir_disable_long_tern_save.clicked.connect(
