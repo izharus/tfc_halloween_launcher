3c2a91d16434921fc436c8d916b20e2e06071e3f
izharus <ruslan.izhakovskij@gmail.com>
2024-11-05 10:16:29 +0300

    feat: show current username on choose server page

--- src/test/launcher/test_main_window.py
+++ src/test/launcher/test_main_window.py
@@ -11,8 +11,13 @@ def test_page_switching_after_success_authentication(
 ):
     """Test if page switches to success after authentication."""
     main_window._config_installer_thread._config_manager = MagicMock()
+
+    main_window._login_widget._auth_data = MagicMock()
+    main_window._login_widget._auth_data.username = "mock_username"
+
     main_window._config_installer_complete()
 
+    assert main_window._ui_instance.label_player_name.text() == "mock_username"
     assert (
         main_window._ui_instance.stackedWidget.currentWidget()
         == main_window._ui_instance.choose_server_page
